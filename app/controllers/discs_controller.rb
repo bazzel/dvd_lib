fb15f97ca1b75599eddb4fafe370a081511558aa
Patrick Baselier <patrick.baselier@gmail.com>
2009-08-29 16:42:10 +0200

    Disc acts as list.

--- app/controllers/discs_controller.rb
+++ app/controllers/discs_controller.rb
@@ -1,6 +1,6 @@
 class DiscsController < ApplicationController
   def index
-    @discs = Disc.all
+    @discs = Disc.all(:order => 'position')
   end
   
   def show
--- app/models/disc.rb
+++ app/models/disc.rb
@@ -1,3 +1,5 @@
 class Disc < ActiveRecord::Base
+  acts_as_list :scope
   attr_accessible :number, :position
+  
 end
